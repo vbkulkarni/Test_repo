6888b684f5ed322e98fb20f600d54d4c3954a406
vbkulkarni <73324381+vbkulkarni@users.noreply.github.com>
2022-08-12 08:29:32 +0530

    Updating Even_no.py

--- Even_No.py
+++ Even_No.py
@@ -1,9 +1,9 @@
 
-num = int (input (“Enter any number to test whether it is odd or even: “)
+num = int (input ('Enter any number to test whether it is odd or even: '))
 
 if (num % 2) == 0:
 
-              print (“The number is even”)
-else
-	print("The number is odd") // odd Statement
+              print (f"The {num} number is even")
+else:
+	print(f"The {num} number is odd") 
               
